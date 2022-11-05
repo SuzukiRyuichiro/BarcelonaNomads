08f2dc5e174c66dcb588c8bf3a6a72b9ea5ef103
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-05 14:14:08 +0100

    Scaffolded with location model

---
+++ BarcelonaNomads/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BarcelonaNomads.Models;
+
+namespace BarcelonaNomads.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<BarcelonaNomads.Models.Location>? Location { get; set; }
+}
+
---
+++ BarcelonaNomads/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BarcelonaNomads.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public Location()
+        { 
+
+        }
+    }
+}
