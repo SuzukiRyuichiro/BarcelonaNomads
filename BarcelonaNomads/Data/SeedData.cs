285676a30aae2f0978035bf41916b1929a5b166c
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-07 09:55:42 +0100

    chore: seed for locations

---
+++ BarcelonaNomads/Data/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BarcelonaNomads.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarcelonaNomads.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (
+                var context = new ApplicationDbContext(
+                    serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()
+                )
+            )
+            {
+                if (!context.Locations.Any())
+                {
+                    context.Locations.AddRange(
+                        entities: new List<Location>()
+                        {
+                            new Location
+                            {
+                                Name = "Santander Work Cafe",
+                                Address = "Passeig de Gràcia, 54, 08007 Barcelona, Spain",
+                                LocationType = LocationType.Cafe
+                            },
+                            new Location
+                            {
+                                Name = "Imagin Cafe",
+                                Address = "C. de Pelai, 11, 08008 Barcelona, Spain",
+                                LocationType = LocationType.Cafe
+                            },
+                            new Location
+                            {
+                                Name = "Monday Barceloneta",
+                                Address = "Pg. de Joan de Borbó, 99, 08039 Barcelona, Spain",
+                                LocationType = LocationType.CoworkingSpace
+                            },
+                            new Location
+                            {
+                                Name = "Muss & Coco",
+                                Address = "C. de les Santjoanistes, 24, 08006 Barcelona, Spain",
+                                LocationType = LocationType.Cafe
+                            }
+                        }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- BarcelonaNomads/Program.cs
+++ BarcelonaNomads/Program.cs
@@ -6,15 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,10 +42,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
-
