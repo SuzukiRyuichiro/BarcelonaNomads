c1a3b882702fcc7c2a7eaa17a7b81e369ff210cd
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-08 18:03:04 +0100

    feat: tooltips and open google maps

--- BarcelonaNomads/wwwroot/js/site.js
+++ BarcelonaNomads/wwwroot/js/site.js
@@ -3,3 +3,9 @@
 
 // Write your JavaScript code.
 
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
